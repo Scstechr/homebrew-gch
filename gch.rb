82cb1e45cf6b721d2a13d266d37c766ca6cb74e1
Scstechr <teufelkonig@gmail.com>
2019-08-23 10:54:42 +0900

    v1.24

--- gch.rb
+++ gch.rb
@@ -2,8 +2,8 @@
 class Gch < Formula
   desc "Git Commit Handler: A tool to handle git related commands easier."
 	homepage "https://github.com/Scstechr/gch"
-  url "https://github.com/Scstechr/gch/releases/download/v1.23/gch-v1.23.tar.gz"
-  sha256 "b9ba074096836a6102cbe96bf9597942047f66f658fda934f5480ba3e7835f3a"
+  url "https://github.com/Scstechr/gch/releases/download/v1.24/gch-v1.24.tar.gz"
+  sha256 "c05f7951df5bdf81153734373db140d8bbdfcc6a4a4db7290077d456a537bf9e"
 
   def install
 		bin.install "gch"
